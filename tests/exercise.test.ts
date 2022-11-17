1bc64335337d7e645e9e42132cb7a0c9ab9e8808
laklov <lasse.klovstad@gmail.com>
2022-11-17 16:55:05 +0100

    Legg til oppgave

--- tests/exercise.test.ts
+++ tests/exercise.test.ts
@@ -1,3 +1,17 @@
+/**
+ Oppgave:
+ 1. Gå inn på https://www.experis.no/
+ 2. Trykk på Stillinger
+ 3. Vent til søkefeltet dukker opp
+ 4. Søk på Senior og trykk enter
+ 5. Gå inn på den første linken som innholder Seniorkonsulent
+ 6. Trykk på knappen “Søk på stillingen”
+ 7. Fyll ut fornavn og etternavn
+ 8. Trykk på søk
+ 9. Forvent og se en av feilmeldingene som dukker opp
+ */
+
+
 import {expect, test} from "@playwright/test";
 
 test.describe.only("Apply job", () => {
